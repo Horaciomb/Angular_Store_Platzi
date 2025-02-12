7a27fbddd6e4f74a87076a0f250c06d9edbd7b61
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-12 15:01:12 -0400

    refactor(product): use inputs for dynamic product data in product component

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  img ='https://picsum.photos/640/640?r='+Math.random()
+  // img ='https://picsum.photos/640/640?r='+Math.random()
+  @Input() img: string = '';
+  @Input() precio: number = 0;
+  @Input() title: string = '';
 }
