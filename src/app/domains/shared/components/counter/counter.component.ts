dc856a4ed9dd22ad7eee5f855d2d2757ade3f86a
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-13 15:17:05 -0400

    utilizando contructor y ngOnChanges

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  @Input({ required: true }) duration: number = 0;
+  @Input({ required: true }) message: string = '';
 
+  constructor() {
+    // No async
+    // this.duration = 12;
+    // Before render
+    console.log('contructor');
+    console.log('-', repeat(10));
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-', repeat(10));
+    console.log(changes);
+  }
 }
