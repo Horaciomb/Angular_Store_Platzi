4bb3967a594b66f2bf43cbe0eb8880039b6d70f6
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-13 14:33:47 -0400

    manejando output desde hijos y padres

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +8,14 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductComponent {
   // img ='https://picsum.photos/640/640?r='+Math.random()
-  @Input({required:true}) img: string = '';
-  @Input({required:true}) precio: number = 0;
-  @Input({required:true}) title: string = '';
+  @Input({ required: true }) img: string = '';
+  @Input({ required: true }) precio: number = 0;
+  @Input({ required: true }) title: string = '';
+
+  @Output() addCart = new EventEmitter();
+
+  addCartHandler() {
+    console.log('Click form child');
+    this.addCart.emit('Hola este es un msg desde el hijo')
+  }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductComponent } from '../../../products/components/product/product.component'
+import { ProductComponent } from '../../../products/components/product/product.component';
 @Component({
   selector: 'app-list',
-  imports: [CommonModule,ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
-
+  fromChild(event: string) {
+    console.log('Estamos en el padre');
+    console.log(event);
+  }
 }
