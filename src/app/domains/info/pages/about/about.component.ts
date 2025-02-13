8d60057bd3dbbd5eaa582e254e048d703f5912f1
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-13 15:53:46 -0400

    añadiendo about page

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CounterComponent } from '../../../shared/components/counter/counter.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CounterComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
+  duration = signal(100);
+  message = signal('Hola');
 
+  changeDuration(event : Event){
+    const input=event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber)
+  }
+  changeMessage(event : Event){
+    const input=event.target as HTMLInputElement;
+    this.message.set(input.value)
+  }
 }
