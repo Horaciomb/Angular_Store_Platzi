0493136896970e24016534b47e6d919580c18bac
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-13 23:16:57 -0400

    añadiendo npm install --save wavesurfer.js y un componente de audio

---
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wave-audio',
+  imports: [],
+  templateUrl: './wave-audio.component.html',
+  styleUrl: './wave-audio.component.css'
+})
+export class WaveAudioComponent {
+
+}
