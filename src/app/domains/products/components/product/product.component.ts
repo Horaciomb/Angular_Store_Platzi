41f038a29cec34b30fd93f4c7210e11d4a25ff2a
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-17 14:32:37 -0400

    manejando produtos como una clase

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -8,14 +9,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ProductComponent {
   // img ='https://picsum.photos/640/640?r='+Math.random()
-  @Input({ required: true }) img: string = '';
-  @Input({ required: true }) precio: number = 0;
-  @Input({ required: true }) title: string = '';
-
+  // @Input({ required: true }) img: string = '';
+  // @Input({ required: true }) precio: number = 0;
+  // @Input({ required: true }) title: string = '';
+  // @Input({ required: true }) creationAt:string = '';
+  @Input({ required: true }) product!:Product
   @Output() addCart = new EventEmitter();
 
   addCartHandler() {
     console.log('Click form child');
-    this.addCart.emit('Hola este es un msg desde el hijo')
+    this.addCart.emit('Hola este es un msg desde el hijo'+ this.product.title)
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../../products/components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 @Component({
   selector: 'app-list',
   imports: [CommonModule, ProductComponent],
@@ -8,6 +9,26 @@ import { ProductComponent } from '../../../products/components/product/product.c
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt:new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=22',
+        creationAt:new Date().toISOString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
   fromChild(event: string) {
     console.log('Estamos en el padre');
     console.log(event);
