d6a4690554ad8c5728b59dba6a4e9c4acf1f774e
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-19 12:30:12 -0400

    agregando servicio de categorías y mejorando la visualización de productos en la lista

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,67 +2,30 @@ import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
-import { HeaderComponent } from '@shared/components/header/header.component';
+import { Category } from '@shared/models/category.models';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
-  // constructor() {
-  //   const initProducts: Product[] = [
-  //     {
-  //       id: Date.now(),
-  //       title: 'Producto 1',
-  //       price: 100,
-  //       image: 'https://picsum.photos/640/640?r=23',
-  //       creationAt: new Date().toISOString(),
-  //     },
-  //     {
-  //       id: Date.now(),
-  //       title: 'Producto 2',
-  //       price: 200,
-  //       image: 'https://picsum.photos/640/640?r=22',
-  //       creationAt: new Date().toISOString(),
-  //     },
-  //     {
-  //       id: Date.now(),
-  //       title: 'Producto 3',
-  //       price: 300,
-  //       image: 'https://picsum.photos/640/640?r=24',
-  //       creationAt: new Date().toISOString(),
-  //     },
-  //     {
-  //       id: Date.now(),
-  //       title: 'Producto 4',
-  //       price: 400,
-  //       image: 'https://picsum.photos/640/640?r=25',
-  //       creationAt: new Date().toISOString(),
-  //     },
-  //     {
-  //       id: Date.now(),
-  //       title: 'Producto 5',
-  //       price: 500,
-  //       image: 'https://picsum.photos/640/640?r=26',
-  //       creationAt: new Date().toISOString(),
-  //     },
-  //     {
-  //       id: Date.now(),
-  //       title: 'Producto 6',
-  //       price: 600,
-  //       image: 'https://picsum.photos/640/640?r=27',
-  //       creationAt: new Date().toISOString(),
-  //     },
-  //   ];
-  //   this.products.set(initProducts);
-  // }
+  private catetegoryService = inject(CategoryService);
   ngOnInit() {
+    this.getProducts();
+    this.getCategories();
+  }
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+  getProducts() {
     this.productService.getProducts().subscribe({
       next: (products) => {
         this.products.set(products);
@@ -70,7 +33,12 @@ export class ListComponent {
       error: () => {},
     });
   }
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  getCategories() {
+    this.catetegoryService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: () => {},
+    });
   }
 }
